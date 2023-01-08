f0fec0d9d476ecfd1e42e3da8456d5175e30223f
Afaq Ahmed <mafaqahmed151@gmail.com>
2023-01-08 18:47:07 +0500

    created pages and routes

--- realtor-clone/src/App.js
+++ realtor-clone/src/App.js
@@ -1,9 +1,48 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-    <h1 className="text-10xl bg-red-400">
-      Hello world!
-    </h1>
+      <Router>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Home/>}
+            />
+            <Route
+              exact
+              path="/profile"
+              element={<Profile/>}
+            />
+            <Route
+              exact
+              path="/sign-in"
+              element={<SignIn/>}
+            />
+            <Route
+              exact
+              path="/sign-up"
+              element={<SignUp/>}
+            />
+            <Route
+              exact
+              path="/forgot-password"
+              element={<ForgotPassword/>}
+            />
+            <Route
+              exact
+              path="/offer"
+              element={<Offer/>}
+            />
+          </Routes>
+        </Router>
     </>
   );
 }
