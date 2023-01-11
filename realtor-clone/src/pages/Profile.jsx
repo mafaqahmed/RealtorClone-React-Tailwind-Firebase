feb14de80bfa1e8f946f307f1f629c65a8818cfd
Afaq Ahmed <mafaqahmed151@gmail.com>
2023-01-11 19:01:29 +0500

    Added sign out functionality

--- realtor-clone/src/pages/Profile.jsx
+++ realtor-clone/src/pages/Profile.jsx
@@ -1,14 +1,21 @@
 import { getAuth } from 'firebase/auth';
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Profile() {
   const auth = getAuth();
+  const navigate = useNavigate();
   const user = auth.currentUser;
   const [formData, setFormData] = useState({
     name: user.displayName,
     email: user.email
   });
   const {name, email} = formData
+
+  const onLoggedOut = () => {
+    auth.signOut();
+    navigate("/");
+  }
   return (
     <>
       <section className='w-full md:w-[40%] flex justify-center items-center flex-col mx-auto'>
@@ -19,7 +26,7 @@ export default function Profile() {
             <input type="text" id='email' value={email} className="w-full rounded bg-white text-gray-700 text-xl border border-gray-300 disabled my-6"/>
             <div className='flex justify-between whitespace-nowrap text-sm md:text-lg'>
               <p>Do you want to change your name? <span className='ml-1 text-red-600 hover:text-red-800 transition ease-in-out cursor-pointer'>Edit</span></p>
-              <p className='ml-1 text-blue-600 hover:text-blue-800 transition ease-in-out cursor-pointer'>Sign out</p>
+              <p onClick={onLoggedOut} className='ml-1 text-blue-600 hover:text-blue-800 transition ease-in-out cursor-pointer'>Sign out</p>
             </div>
           </form>
         </div>
